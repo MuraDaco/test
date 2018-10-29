74dfff431b982619928431deac298b6e2c8f2725
Mura Daco <md120308@gmail.com>
2018-10-29 14:43:42 +0100

    ftr0002: update Qt Platform driver with new heading

--- Platform/Qt/Linux/ArchLinux/Driver/qtButtonsGlo.h
+++ Platform/Qt/Linux/ArchLinux/Driver/qtButtonsGlo.h
@@ -1,3 +1,26 @@
+/******************************************************************************
+ *
+ *   emphiOS - RTOS, User Interface & Business Logic libriaries
+ *   Copyright (C) (2003-2018) Marco Dau
+ *
+ *   'emphiOS' is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   'emphiOS' is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   You can contact me by the following email address
+ *   marco <d o t> ing <d o t> dau <a t> gmail <d o t> com
+ *
+ *****************************************************************************/
+
 
 #ifdef _QT_DEF_CPP
 #define _EXTERN_QT_DEF
